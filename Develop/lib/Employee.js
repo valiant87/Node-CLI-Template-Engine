d6b5f35d7700bd636a8836067a91a03c04ed29cb
Valerian <valnimirenco@gmail.com>
2020-12-09 18:42:54 -0800

    Reinstalled packages, 2 tests passed

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,23 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  // This will set the method and return this.constructor
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
+// This exports the class module
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+// Extends subclass
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email); // this are inherited
+    this.github = github; // this is speciffic to engineer class
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getGithub() {
+    return this.github;
+  }
+}
+// this exports the class module
+
+module.exports = Engineer;
