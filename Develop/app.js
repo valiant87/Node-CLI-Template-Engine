51c31d1b8664a13e0f38d98e945c5e95f97ad012
Val Nimirenco <valnimirenco@gmail.com>
2021-01-17 11:58:01 -0800

    generated team.html

--- Develop/app.js
+++ Develop/app.js
@@ -7,42 +7,154 @@ const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR)
+};
 const render = require("./lib/htmlRenderer");
 
-// This variable  will store new team members
-const newTeamArr = [];
-// Function to add employee, prompt the user using iquirer
+let employees = [];
+let idDb = [];
+
+const validateId = async (data) => {
+  if (idDb.includes(data) === true) {
+    return 'Please enter a unique ID number';
+  }
+  return true;
+};
+
 function addEmployee() {
   inquirer.prompt([
     {
-      type: "list",
-      message: "What employee position would you like to add?",
-      name: "employee",
-      choises: ["Manager", "Engineer", "Intern"],
+      type: 'list',
+      name: 'role',
+      message: 'What kind of employee would you like to add next?',
+      choices: ["Engineer", "Intern"],
     },
-    {
-      type: "input",
-      message: "What is employee's name?",
-      name: "name",
-    },
-    {
-      type: "input",
-      message: "What is the employee's ID?",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is the employee's email?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "What is the Manager's office number?",
-      name: "officeNumber",
-    },
-  ]);
+  ]).then(choice => {
+    switch (choice.role) {
+      case "Engineer":
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "Enter engineer's name"
+          },
+          {
+            type: "number",
+            name: "id",
+            message: "Enter engineer's ID number",
+            validate: validateId
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter engineer's email",
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "Enter engineer's Github username",
+          },
+          {
+            type: "confirm",
+            name: "addAnother",
+            message: "Do you need to add another employee?",
+          }
+        ]).then(data => {
+          const engineer = new Engineer(data.name, data.id, data.email, data.github);
+          employees.push(engineer);
+          idDb.push(data.id);
+          if (data.addAnother === true) {
+            addEmployee();
+          } else {
+            fs.writeFile(outputPath, render(employees), (err) => {
+              if (err) throw err;
+            }
+            );
+            return
+          }
+        })
+        break;
+      case "Intern":
+        inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: "Enter intern's name"
+          },
+          {
+            type: "number",
+            name: "id",
+            message: "Enter intern's ID number",
+            validate: validateId,
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "Enter intern's email",
+          },
+          {
+            type: "input",
+            name: "school",
+            message: "Enter intern's school name",
+          },
+          {
+            type: "confirm",
+            name: "addAnother",
+            message: "Do you need to add another employee?",
+          }
+        ]).then(data => {
+          const intern = new Intern(data.name, data.id, data.email, data.school);
+          employees.push(intern);
+          idDb.push(data.id);
+          if (data.addAnother === true) {
+            addEmployee();
+          }
+          else {
+            fs.writeFile(outputPath, render(employees), (err) => {
+              if (err) throw err;
+            }
+            );
+            return;
+          }
+        })
+        break;
+      default:
+        break;
+    }
+  })
 }
+
+inquirer.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "Enter your name"
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "Enter your ID number",
+    validate: validateId,
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email",
+  },
+  {
+    type: "number",
+    name: "officeNumber",
+    message: "Enter your office number",
+  }
+]).then(data => {
+  const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+  employees.push(manager);
+  idDb.push(data.id);
+  addEmployee();
+
+})
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # Node-CLI-Template-Engine
+---
+### Summary
+---
+[👉 Youtube Project Demo 🎬]()
 
-This is a node CLI that takes in information about employees and generates an HTML webpage that displays summaries for each person.Testing is the main object and all shall pass!
-Using `jest` as a testing system.
+## Table of contents
+
+- [About the project](#About-the-project)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Tests](#Tests)
+- [Questions](#Questions)
+- [License](#License)
+- [Questions](#Questions)
+
+## About the project
+
+This is a node CLI application that takes in  the information about employees and generates an HTML webpage that displays summaries for each person.Testing is the main object and all shall pass.
+Prompting the user will generate a newly formed objects with your inputs
+
+## Installation
+
+Download and install dependencies by running folowing command in your CLI
+` npm i`
+
+## Usage 
+
+One can use this application for a quick and professional team generator aplication using your CLI
+Run the folowing command to start your application
+` node app.js`
+
+## Tests
+
+Run following command to see the if the tests pass
+` npm i run test`
+
+## License
+
+This is licensed under MIT
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Questions
+
+For any questions reach me directly 
+<valnimirenco@gmail.com>
+
+[My repository](http://github.com/valiant87)
